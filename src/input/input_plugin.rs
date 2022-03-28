5ba15faa49a6240795d4b8dc3994cb2256dde6f5
Derhartmannmann <derhartmannmann@gmail.com>
2022-03-28 22:55:04 +0200

    Fix caught problem when normalizing null vector in player movement

--- src/input/input_plugin.rs
+++ src/input/input_plugin.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::{App, Plugin};
 use bevy::render::camera::camera_system;
-use crate::input::enemy_player_collision_system::player_control_system;
+use crate::input::player_control_system::player_control_system;
 use crate::input::setup_player_input::setup_player_input;
 
 pub struct InputPlugin;
