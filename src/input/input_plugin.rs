a02a1a4519102edaa91874bedad5853d2fb2e638
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-03-29 23:55:24 +0200

    Add startup stages and seperate player from camera

--- src/input/input_plugin.rs
+++ src/input/input_plugin.rs
@@ -1,14 +1,15 @@
-use bevy::prelude::{App, Plugin};
+use bevy::prelude::{App, Plugin, StartupStage};
 use bevy::render::camera::camera_system;
 use crate::input::player_control_system::player_control_system;
-use crate::input::setup_player_input::setup_player_input;
+use crate::input::setup_camera_system::setup_camera_system;
+use crate::SetupStages;
 
 pub struct InputPlugin;
 
 impl Plugin for InputPlugin {
     fn build(&self, app: &mut App) {
         app
-            .add_startup_system(setup_player_input)
+            .add_startup_system_to_stage(SetupStages::AfterPlayerSetup, setup_camera_system)
             .add_system(player_control_system);
     }
 }
---
+++ src/input/setup_camera_system.rs
@@ -0,0 +1,30 @@
+use bevy::ecs::system::QuerySingleError;
+use bevy::prelude::*;
+use crate::components::unit_stats_components::{ColliderSize, MoveSpeed, Direction, Damage, Health};
+use crate::collision::collision_components::Collider;
+use crate::components::gun_components::BasicGun;
+
+use crate::components::player_components::MainCamera;
+use crate::Player;
+
+pub fn setup_camera_system(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    player_query: Query<Entity, With<Player>>,
+) {
+    let player_result = match player_query.get_single() {
+        Ok(value) => value,
+        Err(_) => return,
+    };
+
+    let mut camera_bundle = OrthographicCameraBundle::new_2d();
+    camera_bundle.transform.translation.x = 0.0;
+    camera_bundle.transform.translation.y = 0.0;
+    camera_bundle.transform.translation.z = 0.0;
+
+    camera_bundle.orthographic_projection.scale = 5.0;
+
+    let child = commands.spawn_bundle(camera_bundle).insert(MainCamera).id();
+
+    commands.entity(player_result).push_children(&[child]);
+}
