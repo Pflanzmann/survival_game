933159bb861078d41c52589d00fba912b56b4386
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-04-01 01:48:48 +0200

    Add random curve shot turn direction

--- src/bullets/modifications/curve_shot_system.rs
+++ src/bullets/modifications/curve_shot_system.rs
@@ -1,5 +1,6 @@
 use bevy::app::EventReader;
 use bevy::prelude::{Commands, Query, With};
+use rand::random;
 
 use crate::components::event_components::BulletShotEvent;
 use crate::components::modification_components::CurveShot;
@@ -10,15 +11,15 @@ pub fn apply_curved_shot_system(
     mut bullet_shot_event: EventReader<BulletShotEvent>,
 ) {
     for event in bullet_shot_event.iter() {
-        commands.entity(event.entity).insert(CurveShot);
+        commands.entity(event.entity).insert(CurveShot { curve_left: random() });
     }
 }
 
 pub fn curve_shot_system(
-    mut bullet_query: Query<&mut FacingDirection, With<CurveShot>>,
+    mut bullet_query: Query<(&mut FacingDirection, &CurveShot)>,
 ) {
-    for mut direction in bullet_query.iter_mut() {
-        let angle: f32 = 0.005;
+    for (mut direction, curve_shot) in bullet_query.iter_mut() {
+        let angle: f32 = if curve_shot.curve_left { 0.005 } else { -0.005 };
         let x = direction.facing_direction.x;
         let y = direction.facing_direction.y;
 
