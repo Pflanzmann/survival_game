c9e8371d7eaa1f0ef1e969f537b7a33689b1b7d6
Derhartmannmann <derhartmannmann@gmail.com>
2022-03-31 22:46:33 +0200

    Add first iteration of item drops when enemies die

--- src/collision/enemy_bullet_collision_system.rs
+++ src/collision/enemy_bullet_collision_system.rs
@@ -1,13 +1,15 @@
-use bevy::prelude::{Commands, Entity, Query, With};
+use bevy::prelude::{Commands, Entity, EventReader, EventWriter, Query, With};
 use bevy::sprite::collide_aabb::collide;
 
 use crate::{Player, Transform, Without};
 use crate::components::unit_stats_components::{Enemy, UnitSize};
 use crate::components::bullet_components::Bullet;
 use crate::collision::collision_components::Collider;
+use crate::components::event_components::EnemyDiedEvent;
 
 pub fn enemy_bullet_collision_system(
     mut commands: Commands,
+    mut enemy_died_event: EventWriter<EnemyDiedEvent>,
     enemy_query: Query<(Entity, &Transform, &UnitSize), (With<Collider>, With<Enemy>, Without<Bullet>)>,
     bullet_query: Query<(&Transform, &UnitSize), (With<Collider>, With<Bullet>, Without<Enemy>)>,
 ) {
@@ -19,7 +21,9 @@ pub fn enemy_bullet_collision_system(
                 bullet_transform.translation,
                 bullet_size.collider_size,
             ).is_some() {
-                commands.entity(enemy_entity).despawn()
+                commands.entity(enemy_entity).despawn();
+
+                enemy_died_event.send(EnemyDiedEvent{death_position: enemy_transform.translation});
             }
         }
     }
---
+++ src/drops/mod.rs
@@ -0,0 +1,2 @@
+pub mod drops_plugin;
+pub mod basic_drop_system;
