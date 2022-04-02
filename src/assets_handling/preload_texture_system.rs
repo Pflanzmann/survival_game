ba164122de88830c38f6776046a40bd08f47477f
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-04-02 18:34:38 +0200

    Rename json folder

--- src/assets_handling/preload_texture_system.rs
+++ src/assets_handling/preload_texture_system.rs
@@ -2,19 +2,18 @@ use bevy::asset::Handle;
 use bevy::prelude::{AssetServer, Commands, Image, Res, ResMut};
 
 #[derive(Default)]
-pub struct TextureHandles{
+pub struct TextureHandles {
     pub basic_drop_asset_handler: Handle<Image>,
     pub player_sprite: Handle<Image>,
     pub enemy_rock: Handle<Image>,
     pub bullet_fireball: Handle<Image>,
-    pub background_tile: Handle<Image>
+    pub background_tile: Handle<Image>,
 }
 
 pub fn preload_texture_system(
-    mut commands: Commands,
     asset_server: Res<AssetServer>,
-    mut texture_handles : ResMut<TextureHandles>
-){
+    mut texture_handles: ResMut<TextureHandles>,
+) {
     texture_handles.basic_drop_asset_handler = asset_server.load("basic_drop.png");
     texture_handles.player_sprite = asset_server.load("NickelMan.png");
     texture_handles.enemy_rock = asset_server.load("Rock01.png");
