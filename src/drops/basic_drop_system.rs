66cb32617a877b678f7952a3d8ba71ae1c4e693b
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-04-02 18:28:26 +0200

    Remove not needed asset_server usages

--- src/drops/basic_drop_system.rs
+++ src/drops/basic_drop_system.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::{AssetServer, EventReader, Res, Sprite, SpriteBundle, Vec2};
+
 use crate::{Collider, Commands, Transform, UnitSize, Vec3};
 use crate::assets_handling::preload_texture_system::TextureHandles;
 use crate::components::event_components::EnemyDiedEvent;
@@ -7,18 +8,15 @@ use crate::components::item_components::Item;
 pub fn basic_drop_system(
     mut commands: Commands,
     mut enemy_died_event: EventReader<EnemyDiedEvent>,
-    texture_handles : Res<TextureHandles>,
-    asset_server: Res<AssetServer>,
-){
-    for event in enemy_died_event.iter(){
-
+    texture_handles: Res<TextureHandles>,
+) {
+    for event in enemy_died_event.iter() {
         let mut drop_translation = event.death_position;
         drop_translation.z += 1.0;
 
         commands.spawn_bundle(
-            SpriteBundle{
+            SpriteBundle {
                 transform: Transform::from_translation(drop_translation),
-                //texture: asset_server.load("basic_drop.png"),
                 texture: texture_handles.basic_drop_asset_handler.clone(),
                 sprite: Sprite {
                     custom_size: Some(Vec2::new(128.0, 128.0)),
