b97aac76a9957ad3879cdb28f924942e6746d7b5
Derhartmannmann <derhartmannmann@gmail.com>
2022-04-03 15:53:33 +0200

    Fix little code cleanup

--- src/drops/item_pickup_system.rs
+++ src/drops/item_pickup_system.rs
@@ -5,16 +5,11 @@ use crate::components::event_components::{BulletStoppedEvent, ItemPickupEvent};
 use crate::Res;
 
 pub fn item_pickup_system(
-    mut commands: Commands,
     mut item_pickup_event: EventReader<ItemPickupEvent>,
     asset_server : Res<AssetServer>,
     audio : Res<Audio>
 ){
     for event in item_pickup_event.iter() {
-        //sound abspielen
-        //counter hochzählen
-        //asset_server.load("coin_pickup_sound.ogg");
         audio.play(asset_server.load("coin_pickup_sound.ogg"));
-        println!("something event fires");
     }
 }
