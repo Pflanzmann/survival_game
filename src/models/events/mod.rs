6ab34d877ce3913c7b0c2f988c1a969088e6c98a
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-04-05 20:49:21 +0200

    Add PlayerEnemyCollisionEvent and PlayerDiedEvent

--- src/models/events/mod.rs
+++ src/models/events/mod.rs
@@ -5,12 +5,16 @@ use crate::models::events::bullet_shot_event::BulletShotEvent;
 use crate::models::events::bullet_stopped_event::BulletStoppedEvent;
 use crate::models::events::enemy_died_event::EnemyDiedEvent;
 use crate::models::events::item_collision_event::ItemCollisionEvent;
+use crate::models::events::player_died_event::PlayerDiedEvent;
+use crate::models::events::player_enemy_collision_event::PlayerEnemyCollisionEvent;
 
 pub mod bullet_shot_event;
 pub mod enemy_died_event;
 pub mod bullet_enemy_collision_event;
 pub mod bullet_stopped_event;
 pub mod item_collision_event;
+pub mod player_enemy_collision_event;
+pub mod player_died_event;
 
 pub struct EventsPlugin;
 
@@ -21,6 +25,8 @@ impl Plugin for EventsPlugin {
             .add_event::<ItemCollisionEvent>()
             .add_event::<BulletShotEvent>()
             .add_event::<BulletEnemyCollisionEvent>()
-            .add_event::<BulletStoppedEvent>();
+            .add_event::<BulletStoppedEvent>()
+            .add_event::<PlayerEnemyCollisionEvent>()
+            .add_event::<PlayerDiedEvent>();
     }
 }
