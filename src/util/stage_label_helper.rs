3d0b813c26d13248bae54982deeefe517d95c764
Derhartmannmann <derhartmannmann@gmail.com>
2022-04-10 19:57:20 +0200

    Refactor replaced strings in Schedulelabels by enum and cleaned up some prints

--- src/util/stage_label_helper.rs
+++ src/util/stage_label_helper.rs
@@ -1,35 +1,36 @@
 use bevy::prelude::SystemSet;
+use crate::navigation::ScheduleLabel;
 
 pub fn in_first(mut set: SystemSet) -> SystemSet {
     set
-        .before("first")
+        .before(ScheduleLabel::First)
 }
 
 pub fn in_collision(mut set: SystemSet) -> SystemSet {
     set
-        .before("collision")
-        .after("first")
+        .before(ScheduleLabel::Collision)
+        .after(ScheduleLabel::First)
 }
 
 pub fn in_pre_update(mut set: SystemSet) -> SystemSet {
     set
-        .before("preUpdate")
-        .after("collision")
+        .before(ScheduleLabel::PreUpdate)
+        .after(ScheduleLabel::Collision)
 }
 
 pub fn in_update(mut set: SystemSet) -> SystemSet {
     set
-        .before("Update")
-        .after("preUpdate")
+        .before(ScheduleLabel::Update)
+        .after(ScheduleLabel::PreUpdate)
 }
 
 pub fn in_post_update(mut set: SystemSet) -> SystemSet {
     set
-        .before("PostUpdate")
-        .after("Update")
+        .before(ScheduleLabel::PostUpdate)
+        .after(ScheduleLabel::Update)
 }
 
 pub fn in_last(mut set: SystemSet) -> SystemSet {
     set
-        .after("PostUpdate")
+        .after(ScheduleLabel::PostUpdate)
 }
