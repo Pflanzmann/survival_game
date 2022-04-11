d24693344328c4f6fa0a15c33738ab26f394a1d7
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-04-11 21:20:30 +0200

    Add apply_mod system

--- src/units/modifications/apply_bullet_mod_to_target_system.rs
+++ src/units/modifications/apply_bullet_mod_to_target_system.rs
@@ -1,18 +1,34 @@
-use crate::{Commands, Component, EventReader, Query, With};
+use bevy::prelude::Without;
+
+use crate::{Commands, Component, EventReader, Player, Query, With};
 use crate::models::events::apply_mod_to_target_event::ApplyModToTargetSystem;
+use crate::models::gun_components::WeaponSlot;
 use crate::models::modification_attributes::modification::Modification;
+use crate::models::modification_components::ModContainerSlot;
 
 pub fn apply_bullet_mod_to_target_system<T: Component + Clone>(
     mut commands: Commands,
     mut apply_events: EventReader<ApplyModToTargetSystem>,
     mod_query: Query<&T, With<Modification>>,
+    player_query: Query<&WeaponSlot, With<Player>>,
+    gun_query: Query<&ModContainerSlot, Without<Player>>,
 ) {
     for apply_event in apply_events.iter() {
-        let player_mod = match mod_query.get(apply_event.mod_entity) {
+        let bullet_mod = match mod_query.get(apply_event.mod_entity) {
             Ok(modification) => modification,
             Err(_) => continue,
         };
 
-        commands.entity(apply_event.target_entity).insert(player_mod.clone());
+        let player_weapon_slot = match player_query.get(apply_event.target_entity) {
+            Ok(modification) => modification,
+            Err(_) => continue,
+        };
+
+        let weapon_mod_container = match gun_query.get(player_weapon_slot.weapon_entity) {
+            Ok(modification) => modification,
+            Err(_) => continue,
+        };
+
+        commands.entity(weapon_mod_container.container_entity).insert(bullet_mod.clone());
     }
 }
