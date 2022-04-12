4068d16ef1d26087273c32d33e24484986c1d421
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-04-12 22:49:23 +0200

    Remove warning

--- src/util/stage_label_helper.rs
+++ src/util/stage_label_helper.rs
@@ -1,36 +1,37 @@
 use bevy::prelude::SystemSet;
+
 use crate::navigation::ScheduleLabel;
 
-pub fn in_first(mut set: SystemSet) -> SystemSet {
+pub fn in_first(set: SystemSet) -> SystemSet {
     set
         .before(ScheduleLabel::First)
 }
 
-pub fn in_collision(mut set: SystemSet) -> SystemSet {
+pub fn in_collision(set: SystemSet) -> SystemSet {
     set
         .before(ScheduleLabel::Collision)
         .after(ScheduleLabel::First)
 }
 
-pub fn in_pre_update(mut set: SystemSet) -> SystemSet {
+pub fn in_pre_update(set: SystemSet) -> SystemSet {
     set
         .before(ScheduleLabel::PreUpdate)
         .after(ScheduleLabel::Collision)
 }
 
-pub fn in_update(mut set: SystemSet) -> SystemSet {
+pub fn in_update(set: SystemSet) -> SystemSet {
     set
         .before(ScheduleLabel::Update)
         .after(ScheduleLabel::PreUpdate)
 }
 
-pub fn in_post_update(mut set: SystemSet) -> SystemSet {
+pub fn in_post_update(set: SystemSet) -> SystemSet {
     set
         .before(ScheduleLabel::PostUpdate)
         .after(ScheduleLabel::Update)
 }
 
-pub fn in_last(mut set: SystemSet) -> SystemSet {
+pub fn in_last(set: SystemSet) -> SystemSet {
     set
         .after(ScheduleLabel::PostUpdate)
 }
