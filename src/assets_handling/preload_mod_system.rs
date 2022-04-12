7a6889ad7254eb51244011cdb5c85d6f1414db70
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-04-12 20:49:23 +0200

    Implement AffectMoveSpeed

--- src/assets_handling/preload_mod_system.rs
+++ src/assets_handling/preload_mod_system.rs
@@ -5,17 +5,18 @@ use bevy::prelude::{BuildChildren, Name};
 use serde::de::DeserializeOwned;
 
 use crate::{Commands, Entity, EntityCommands, ResMut};
-use crate::models::modifications::grow_shot::GrowShot;
-use crate::models::unit_attributes::damage::Damage;
-use crate::models::unit_attributes::health::Health;
-use crate::models::unit_attributes::move_speed::MoveSpeed;
-use crate::models::modifications::descriptors::modification::Modification;
+use crate::models::modifications::affects::affect_move_speed::AffectMoveSpeed;
 use crate::models::modifications::curve_shot::CurveShot;
 use crate::models::modifications::descriptors::mod_name::ModName;
 use crate::models::modifications::descriptors::mod_sprite_handler::{ModSpriteHandler, ModSpriteHandlerHelper};
+use crate::models::modifications::descriptors::modification::Modification;
 use crate::models::modifications::descriptors::tool_tip::ToolTip;
+use crate::models::modifications::grow_shot::GrowShot;
 use crate::models::modifications::split_shot::SplitShot;
 use crate::models::modifications::sprinting::Sprinting;
+use crate::models::unit_attributes::damage::Damage;
+use crate::models::unit_attributes::health::Health;
+use crate::models::unit_attributes::move_speed::MoveSpeed;
 use crate::util::read_file_to_string::read_file_to_string;
 
 pub fn preload_mod_system(
@@ -60,6 +61,8 @@ pub fn spawn_entity(
                 entity.insert(ModSpriteHandler { sprite: asset_server.load(&helper.sprite) });
             }
             "ToolTip" => { entity.insert(get_component::<ToolTip>(object_data.clone())); }
+
+            "AffectMoveSpeed" => { entity.insert(get_component::<AffectMoveSpeed>(object_data.clone())); }
 
             "Health" => { entity.insert(get_component::<Health>(object_data.clone())); }
             "Damage" => { entity.insert(get_component::<Damage>(object_data.clone())); }
--- src/units/unit_modifications/apply_affect_system.rs
+++ src/units/unit_modifications/apply_affect_system.rs
@@ -5,12 +5,12 @@ use crate::models::unit_attributes::attribute::Attribute;
 use crate::models::events::apply_mod_to_target_event::ApplyModToTargetSystem;
 use crate::models::modifications::affects::attribute_affect::AttributeAffect;
 
-pub fn apply_attribute_system<
+pub fn apply_affect_system<
     T: Component + Attribute,
     U: Component + AttributeAffect<T>>(
     mut apply_events: EventReader<ApplyModToTargetSystem>,
     mut target_query: Query<&mut T>,
-    mod_query: Query<&U>,
+    affect_query: Query<&U>,
 ) {
     for apply_event in apply_events.iter() {
         let mut target_attribute = match target_query.get_mut(apply_event.target_entity) {
@@ -18,11 +18,11 @@ pub fn apply_attribute_system<
             Err(_) => continue,
         };
 
-        let modification = match mod_query.get(apply_event.mod_entity) {
-            Ok(modification) => modification,
+        let affect = match affect_query.get(apply_event.mod_entity) {
+            Ok(affect) => affect,
             Err(_) => continue,
         };
 
-        modification.add_affect(&mut target_attribute);
+        affect.add_affect(&mut target_attribute);
     }
 }
