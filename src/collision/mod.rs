c19294ef329a22d7715135987f0932f92a0c84da
Derhartmannmann <derhartmannmann@gmail.com>
2022-04-13 22:17:20 +0200

    Add collision docs

--- src/collision/mod.rs
+++ src/collision/mod.rs
@@ -13,6 +13,13 @@ mod enemy_bullet_collision_system;
 mod enemy_enemy_collision_system;
 mod item_player_collision_system;
 
+/// this has system running to check for collision between different game objects
+///
+/// in order to not loose any collision events, These are created in a separate "stage" before
+/// the regular update ticks. This scheduling is also necessary to order the firing and reaction
+/// to events as well as possible despawns of entities. Otherwise we ran into problems with events
+/// that try to react on entities that don't exist anymore
+
 pub struct CollisionPlugin;
 
 const FIXED_TIMESTEP: f64 = 0.1;
