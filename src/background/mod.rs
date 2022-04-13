c494184c5a82ae8473b79cfc14bb7004908f821f
Derhartmannmann <derhartmannmann@gmail.com>
2022-04-13 21:54:57 +0200

    Update added more docs

--- src/background/mod.rs
+++ src/background/mod.rs
@@ -8,6 +8,13 @@ use crate::util::stage_label_helper::in_update;
 mod background_startup_system;
 mod move_background_tiles_system;
 
+/// this plugin manages the "world" spawning and procedural world building
+///
+/// [ background_startup_system ] spawns the initial world tiles
+///
+/// [ move_background_tiles_system ] moves the background tiles when the player moves into
+/// a new "chunk"
+
 pub struct BackgroundPlugin;
 
 impl Plugin for BackgroundPlugin {
