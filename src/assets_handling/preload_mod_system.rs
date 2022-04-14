a690f261632f00122faa4280c67b49c1ecea6fc1
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-04-15 00:47:17 +0200

    Resolve warnings

--- src/assets_handling/preload_mod_system.rs
+++ src/assets_handling/preload_mod_system.rs
@@ -1,17 +1,17 @@
-use bevy::prelude::{BuildChildren, Commands, Entity, Name, NonSend, Res};
+use bevy::prelude::{BuildChildren, Commands, Entity, Name, NonSend};
 
 use crate::util::entity_builder::EntityBuilder;
 
 pub fn preload_mod_system(
     mut commands: Commands,
-    mut entity_builder: NonSend<EntityBuilder>,
+    entity_builder: NonSend<EntityBuilder>,
 ) {
-    let mut children: Vec<Entity> = Vec::new();
-
-    children.push(entity_builder.spawn_entity(&mut commands, "configurations/mod_configurations/grow_shot_config.json"));
-    children.push(entity_builder.spawn_entity(&mut commands, "configurations/mod_configurations/curve_shot_config.json"));
-    children.push(entity_builder.spawn_entity(&mut commands, "configurations/mod_configurations/split_shot_config.json"));
-    children.push(entity_builder.spawn_entity(&mut commands, "configurations/mod_configurations/sprinting_config.json"));
+    let children: Vec<Entity> = vec![
+        entity_builder.spawn_entity(&mut commands, "configurations/mod_configurations/grow_shot_config.json"),
+        entity_builder.spawn_entity(&mut commands, "configurations/mod_configurations/curve_shot_config.json"),
+        entity_builder.spawn_entity(&mut commands, "configurations/mod_configurations/split_shot_config.json"),
+        entity_builder.spawn_entity(&mut commands, "configurations/mod_configurations/sprinting_config.json"),
+    ];
 
     let parent = commands.spawn().insert(Name::new("Mod Entities")).id();
     commands.entity(parent).push_children(&*children);
