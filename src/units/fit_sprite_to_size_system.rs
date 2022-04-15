d48e3ca6b22e9e39e1d74db222fcb4f8e55408c5
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-04-15 23:42:31 +0200

    Resolve typo

--- src/units/fit_sprite_to_size_system.rs
+++ src/units/fit_sprite_to_size_system.rs
@@ -5,7 +5,7 @@ use crate::models::unit_size::UnitSize;
 pub fn fit_sprite_to_size_system(
     mut target_query: Query<(&mut Sprite, &UnitSize), Changed<UnitSize>>
 ) {
-    for (mut sprite, unit_site) in target_query.iter_mut() {
-        sprite.custom_size = Option::Some(unit_site.collider_size);
+    for (mut sprite, unit_size) in target_query.iter_mut() {
+        sprite.custom_size = Option::Some(unit_size.collider_size);
     }
 }
