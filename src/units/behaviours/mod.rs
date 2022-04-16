eecaa36ad7f8e0e998275b447ff6aa2843972731
Derhartmannmann <derhartmannmann@gmail.com>
2022-04-16 22:31:38 +0200

    Add implemented teleport_to_target_behavior_system.rs and have turret_update_system.rs use it

--- src/units/behaviours/mod.rs
+++ src/units/behaviours/mod.rs
@@ -3,9 +3,11 @@ use bevy::prelude::{App, Plugin, SystemSet};
 
 use crate::AppState;
 use crate::units::behaviours::spin_aim_behaviour_system::spin_aim_behaviour_system;
+use crate::units::behaviours::teleport_to_target_behavior_system::teleport_to_target_behavior_system;
 use crate::util::stage_label_helper::in_update;
 
 mod spin_aim_behaviour_system;
+mod teleport_to_target_behavior_system;
 
 pub struct BehaviourPlugin;
 
@@ -17,6 +19,7 @@ impl Plugin for BehaviourPlugin {
                     SystemSet::on_update(AppState::InGame)
                         .with_run_criteria(FixedTimestep::step(0.2))
                         .with_system(spin_aim_behaviour_system)
+                        .with_system(teleport_to_target_behavior_system)
                 )
             );
     }
