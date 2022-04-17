e8cc87f462fe3e94e992fae7c20a02eb54152916
Derhartmannmann <derhartmannmann@gmail.com>
2022-04-17 14:58:16 +0200

    Update changed teleport_to_target_behavior component to control behavior

--- src/units/behaviours/teleport_to_target_behavior_system.rs
+++ src/units/behaviours/teleport_to_target_behavior_system.rs
@@ -1,17 +1,17 @@
 use bevy::prelude::{Entity, Query, Transform, With, Without};
 
-use crate::models::behaviour::teleport_to_target_behavior::{TeleportTargetPointer, TeleportToTargetBehavior};
+use crate::models::behaviour::teleport_to_target_behavior::{TeleportToTargetBehavior};
 use crate::models::player::Player;
 use crate::util::get_close_position_2d::get_close_position_2d;
 
 pub fn teleport_to_target_behavior_system(
-    mut unit_query: Query<(&TeleportTargetPointer, &mut Transform), (With<TeleportToTargetBehavior>, Without<Player>)>,
+    mut unit_query: Query<(&TeleportToTargetBehavior, &mut Transform), Without<Player>>,
     target_query: Query<(Entity, &Transform), With<Player>>,
 ) {
     for (player_entity, player_transform) in target_query.iter() {
-        for (owner, mut turret_transform) in unit_query.iter_mut() {
-            if player_entity == owner.target && player_transform.translation.distance(turret_transform.translation) > 2000.0 {
-                let pos_vec = get_close_position_2d(*player_transform);
+        for (target_behavior, mut turret_transform) in unit_query.iter_mut() {
+            if player_entity == target_behavior.target && player_transform.translation.distance(turret_transform.translation) > target_behavior.distance {
+                let pos_vec = get_close_position_2d(*player_transform, target_behavior.proximity_min, target_behavior.proximity_max);
 
                 turret_transform.translation.x = pos_vec[0];
                 turret_transform.translation.y = pos_vec[1];
