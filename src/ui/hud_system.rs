046054c3412bdd86525d45d0a7003f387d167ad2
Daniel Hartmann <daniel.hartmann@gemo.de>
2022-04-19 22:52:02 +0200

    Update prepared mod_hud to be filled with data (sprites) from modregister component on player

--- src/ui/hud_system.rs
+++ src/ui/hud_system.rs
@@ -1,10 +1,14 @@
 use bevy::prelude::*;
 
-use crate::{GoldWallet,TextureHandles};
+use crate::{GoldWallet, TextureHandles};
 use crate::models::modifications::grow_shot::GrowShot;
 use crate::models::mod_container::ModContainer;
+use crate::models::mod_register::ModRegister;
 use crate::models::modifications::curve_shot::CurveShot;
+use crate::models::modifications::descriptors::mod_sprite_path::ModSpritePath;
+use crate::models::modifications::descriptors::modification::Modification;
 use crate::models::modifications::split_shot::SplitShot;
+use crate::models::player::Player;
 use crate::models::ui_components::hud::{BulletHud, CoinText};
 
 
@@ -37,24 +41,24 @@ pub fn spawn_text_system(
                 horizontal: HorizontalAlign::Center,
             },
         ),*/
-        text : Text{
-            sections : vec![
-                TextSection{
+        text: Text {
+            sections: vec![
+                TextSection {
                     value: "Coins: ".to_string(),
-                    style: TextStyle{
+                    style: TextStyle {
                         font: asset_loader.load("fonts/BodoniFLF-Roman.ttf"),
                         font_size: 60.0,
                         color: Color::WHITE,
-                    }
+                    },
                 },
-                TextSection{
+                TextSection {
                     value: "".to_string(),
-                    style: TextStyle{
+                    style: TextStyle {
                         font: asset_loader.load("fonts/BodoniFLF-Roman.ttf"),
                         font_size: 60.0,
                         color: Color::WHITE,
-                    }
-                }
+                    },
+                },
             ],
             ..Default::default()
         },
@@ -63,7 +67,7 @@ pub fn spawn_text_system(
         .insert(CoinText)
         .id();
 
-        //modification hud
+    //modification hud
     commands.spawn_bundle(NodeBundle {
         style: Style {
             size: Size::new(Val::Percent(25.0), Val::Percent(5.0)),
@@ -147,20 +151,21 @@ pub fn update_text_system(
 
 pub fn update_bullet_hud_system(
     mut commands: Commands,
+    asset_server : Res<AssetServer>,
     texture_handles: Res<TextureHandles>,
-
-    mut containter_query: Query<(Option<&SplitShot>, Option<&CurveShot>, Option<&GrowShot>), (With<ModContainer>, Changed<SplitShot>)>,
+    //mut containter_query: Query<(Option<&SplitShot>, Option<&CurveShot>, Option<&GrowShot>), (With<ModContainer>, Changed<SplitShot>)>,
     mut hud_query: Query<Entity, With<BulletHud>>,
-
+    mut player_query: Query<(Entity, &mut ModRegister), With<Player>>,
+    mut mod_query : Query<(Entity, &ModSpritePath), With<Modification>>
 ) {
-    for entity in hud_query.iter_mut() {
+    /*for entity in hud_query.iter_mut() {
         for (split, curve, grow) in containter_query.iter_mut() {
             if split.is_some() {
                 commands.entity(entity).with_children(|parent| {
                     parent.spawn_bundle(ImageBundle {
-                        image : texture_handles.bullet_fireball.clone().into(),
-                        style :Style{
-                            size : Size::new(Val::Percent(10.0), Val::Percent(80.0)),
+                        image: texture_handles.bullet_fireball.clone().into(),
+                        style: Style {
+                            size: Size::new(Val::Percent(10.0), Val::Percent(80.0)),
                             ..Default::default()
                         },
                         ..Default::default()
@@ -168,5 +173,58 @@ pub fn update_bullet_hud_system(
                 }).id();
             } else {}
         }
+    }*/
+for (player_entity, mod_reg) in player_query.iter(){
+    for hud_entity in hud_query.iter(){
+        for some in mod_reg.register.iter(){
+            let (any, sprite) = match mod_query.get(*some){
+                Ok(value) => value,
+                Err(_) => continue,
+            };
+            commands.entity(hud_entity).with_children(|parent| {
+                parent.spawn_bundle(ImageBundle {
+                    image: asset_server.load(&sprite.path).into(),
+                    style: Style {
+                        size: Size::new(Val::Percent(10.0), Val::Percent(80.0)),
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                });
+            }).id();
+        }
     }
+}
+    /*
+     let enemy_position = match enemy_query.get(event.enemy_entity) {
+            Ok(transform) => transform.translation,
+            Err(_) => continue,
+        };
+     */
+
+
+
+
+    //query all mod_entities ->
+    // for ... in mod_reg.iter()
+    //      for (mod_entity, sprite) in mod_entities.get(modreg.regster)
+
+    /*for (entity, mut mod_reg) in player_query.iter() {
+        for entity in hud_query.iter_mut() {
+            let mut i = 0;
+            for some in mod_reg.register.iter() {
+                println!("{}", some);
+                i += 1;
+                commands.entity(entity).with_children(|parent| {
+                    parent.spawn_bundle(ImageBundle {
+                        image: texture_handles.bullet_fireball.clone().into(),
+                        style: Style {
+                            size: Size::new(Val::Percent(10.0), Val::Percent(80.0)),
+                            ..Default::default()
+                        },
+                        ..Default::default()
+                    });
+                }).id();
+            }
+        }
+    }*/
 }
