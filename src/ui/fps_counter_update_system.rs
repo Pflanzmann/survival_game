3a933231ae0609e38bc83c188dfa7358205a257a
Lord Lothar Neumann <lotharneumann90@web.de>
2022-04-20 22:16:09 +0200

    Add FPS counter on screen

---
+++ src/ui/fps_counter_update_system.rs
@@ -0,0 +1,14 @@
+use bevy::prelude::{Query, Res, Text, Time, With};
+
+use crate::models::ui_components::hud::CoinText;
+
+pub fn fps_counter_update_system(
+    mut text_query: Query<&mut Text, With<CoinText>>,
+    time: Res<Time>,
+) {
+    if time.is_changed() {
+        for mut text in text_query.iter_mut() {
+            text.sections[0].value = format!("{:#?}", 1.0 / time.delta_seconds());
+        }
+    }
+}
