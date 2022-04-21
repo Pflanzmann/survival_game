d892f3136d118aff876667a89c5ceaf4584b70c8
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-04-22 00:21:23 +0200

    Implement first iteration of a quad tree

--- src/collision/mod.rs
+++ src/collision/mod.rs
@@ -2,16 +2,22 @@ use bevy::core::FixedTimestep;
 use bevy::prelude::{Plugin, SystemSet};
 
 use crate::{App, AppState};
+use crate::collision::calculate_quad_tree_system::calculate_quad_tree_system;
 use crate::collision::enemy_bullet_collision_system::enemy_bullet_collision_system;
 use crate::collision::enemy_enemy_collision_system::enemy_enemy_collision_system;
+use crate::collision::enemy_enemy_collision_system_old::enemy_enemy_collision_system_old;
 use crate::collision::enemy_player_collision_system::enemy_player_collision_system;
 use crate::collision::item_player_collision_system::item_player_collision_system;
-use crate::util::stage_label_helper::in_collision;
+use crate::collision::quad_tree::Quadtree;
+use crate::util::stage_label_helper::{in_collision, in_update};
 
 mod enemy_player_collision_system;
 mod enemy_bullet_collision_system;
 mod enemy_enemy_collision_system;
 mod item_player_collision_system;
+mod calculate_quad_tree_system;
+mod quad_tree;
+mod enemy_enemy_collision_system_old;
 
 /// this has system running to check for collision between different game objects
 ///
@@ -24,17 +30,33 @@ pub struct CollisionPlugin;
 
 const FIXED_TIMESTEP: f64 = 0.02;
 
+#[derive(Default)]
+pub struct QuadTreeHolder {
+    quad_tree: Quadtree,
+}
+
 impl Plugin for CollisionPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system_set(
-            in_collision(
-                SystemSet::on_update(AppState::InGame)
-                    .with_run_criteria(FixedTimestep::step(FIXED_TIMESTEP))
-                    .with_system(enemy_player_collision_system)
-                    .with_system(enemy_bullet_collision_system)
-                    .with_system(enemy_enemy_collision_system)
-                    .with_system(item_player_collision_system),
+        app
+            .init_resource::<QuadTreeHolder>()
+
+            .add_system_set(
+                in_collision(
+                    SystemSet::on_update(AppState::InGame)
+                        .with_system(enemy_player_collision_system)
+                        .with_system(enemy_bullet_collision_system)
+                        .with_system(enemy_enemy_collision_system)
+                        .with_system(enemy_enemy_collision_system_old)
+                        .with_system(item_player_collision_system)
+                )
             )
-        );
+
+            .add_system_set(
+                in_update(
+                    SystemSet::on_update(AppState::InGame)
+                        .with_system(calculate_quad_tree_system)
+                )
+            )
+        ;
     }
 }
