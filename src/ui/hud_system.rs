6057cf910d8ab4a53f99c1d2e977726f16b38cf1
Derhartmannmann <derhartmannmann@gmail.com>
2022-04-23 10:56:22 +0200

    Upadate bullet hud now correctly displays current mods

--- src/ui/hud_system.rs
+++ src/ui/hud_system.rs
@@ -2,17 +2,16 @@ use bevy::prelude::*;
 
 use crate::{GoldWallet, TextureHandles};
 use crate::models::events::apply_mod_to_target_event::ApplyModToTargetEvent;
-use crate::models::modifications::grow_shot::GrowShot;
 use crate::models::mod_container::ModContainer;
 use crate::models::mod_register::ModRegister;
 use crate::models::modifications::curve_shot::CurveShot;
 use crate::models::modifications::descriptors::mod_name::ModName;
 use crate::models::modifications::descriptors::mod_sprite_path::ModSpritePath;
 use crate::models::modifications::descriptors::modification::Modification;
+use crate::models::modifications::grow_shot::GrowShot;
 use crate::models::modifications::split_shot::SplitShot;
 use crate::models::player::Player;
 use crate::models::ui_components::hud::{BulletHud, CoinText};
-
 
 pub fn spawn_text_system(
     mut commands: Commands,
@@ -21,6 +20,7 @@ pub fn spawn_text_system(
 ) {
     coin_counter.number = 0;
 
+    // coin counter
     commands.spawn_bundle(TextBundle {
         style: Style {
             position_type: PositionType::Absolute,
@@ -66,8 +66,7 @@ pub fn spawn_text_system(
         },
         ..Default::default()
     })
-        .insert(CoinText)
-        .id();
+        .insert(CoinText);
 
     //modification hud
     commands.spawn_bundle(NodeBundle {
@@ -112,32 +111,25 @@ pub fn spawn_text_system(
             ..Default::default()
         });
     }).with_children(|parent| {
-        parent.spawn_bundle(TextBundle {
-            /*style: Style {
-                position_type: PositionType::Absolute,
+        parent.spawn_bundle(NodeBundle {
+            style: Style {
+                size: Size::new(Val::Percent(40.0), Val::Percent(95.0)),
                 position: Rect {
                     left: Val::Percent(5.0),
                     top: Val::Percent(2.0),
                     ..Default::default()
                 },
+                position_type: PositionType::Relative,
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::FlexStart,
+                flex_direction: FlexDirection::Row,
                 ..Default::default()
-            },*/
-            text: Text::with_section(
-                "Modliste ".to_string(),
-                TextStyle {
-                    font: asset_loader.load("fonts/BodoniFLF-Roman.ttf"),
-                    font_size: 30.0,
-                    color: Color::BLACK,
-                },
-                TextAlignment {
-                    vertical: VerticalAlign::Center,
-                    horizontal: HorizontalAlign::Center,
-                },
-            ),
+            },
+            color: Color::NONE.into(),
             ..Default::default()
-        });
-    })
-        .insert(BulletHud);
+        })
+            .insert(BulletHud);
+    });
 }
 
 pub fn update_text_system(
@@ -160,8 +152,11 @@ pub fn update_bullet_hud_system(
     mut player_query: Query<(Entity, &mut ModRegister), (With<Player>, Changed<ModRegister>)>,
     mut mod_query: Query<(Entity, &ModSpritePath, &ModName), With<Modification>>,
 ) {
-    //reset the hud whenever a change is registered
-    //also have the changed<modregister> as run criteria
+    for entity in hud_query.iter() {
+        for (player_entity, mod_reg) in player_query.iter() {
+            commands.entity(entity).despawn_descendants();
+        }
+    }
     for (player_entity, mod_reg) in player_query.iter() {
         for hud_entity in hud_query.iter() {
             for some in mod_reg.register.iter() {
@@ -182,7 +177,7 @@ pub fn update_bullet_hud_system(
                         },
                         ..Default::default()
                     });
-                }).id();
+                });
             }
         }
     }
