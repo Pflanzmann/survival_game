56dccfa510dce8dac95fe204c31dfec225842ff6
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-04-30 19:38:18 +0200

    Implement SteeringBehavior

--- src/models/behavior/mod.rs
+++ src/models/behavior/mod.rs
@@ -4,3 +4,4 @@ pub mod chase_target_behavior;
 pub mod mono_directional_move_behavior;
 pub mod aim_at_closest_target_behavior;
 pub mod rotate_behavior;
+pub mod steering_behavior;
--- src/units/move_unit_system.rs
+++ src/units/move_unit_system.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::{Query, Res, Time, Transform};
+use crate::models::behavior::steering_behavior::SteeringBehavior;
 
 use crate::models::move_direction::MoveDirection;
 use crate::models::unit_attributes::attribute::Attribute;
@@ -7,17 +8,21 @@ use crate::models::unit_attributes::travel_range::TravelRange;
 
 pub fn move_unit_system(
     time: Res<Time>,
-    mut moving_entities: Query<(&mut Transform, &MoveDirection, &MoveSpeed, Option<&mut TravelRange>)>,
+    mut moving_entities: Query<(&mut Transform, &MoveDirection, &MoveSpeed, Option<&mut TravelRange>, Option<&SteeringBehavior>)>,
 ) {
-    for (mut transform, direction, move_speed, travel_range) in moving_entities.iter_mut() {
-        transform.translation.x += direction.direction.x * move_speed.get_total_amount() * time.delta_seconds() * 60.0;
-        transform.translation.y += direction.direction.y * move_speed.get_total_amount() * time.delta_seconds() * 60.0;
-
-        let mut travel_range = match travel_range {
-            Some(value) => value,
-            None => continue,
+    for (mut transform, direction, move_speed, travel_range, steering_behavior) in moving_entities.iter_mut() {
+        let direction =  if let Some(steering_behavior) = steering_behavior {
+            (direction.direction + steering_behavior.direction.extend(0.0)).normalize_or_zero()
+        } else {
+            direction.direction
         };
 
-        travel_range.distance_traveled += move_speed.get_total_amount() * time.delta_seconds() * 60.0
+        transform.translation.x += direction.x * move_speed.get_total_amount() * time.delta_seconds() * 60.0;
+        transform.translation.y += direction.y * move_speed.get_total_amount() * time.delta_seconds() * 60.0;
+
+
+        if let Some(mut travel_range) = travel_range {
+            travel_range.distance_traveled += move_speed.get_total_amount() * time.delta_seconds() * 60.0
+        };
     }
 }
