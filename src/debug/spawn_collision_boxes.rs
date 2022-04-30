9875cb4e3fb103b1c7522d0f0ae433ea55e749e9
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-04-30 15:47:13 +0200

    Add visuals for round collider

--- src/debug/spawn_collision_boxes.rs
+++ src/debug/spawn_collision_boxes.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+use crate::models::collider::collider_type::ColliderType;
+use crate::models::collider::collider_type::ColliderType::{Circle, Rectangle};
 use crate::models::unit_size::UnitSize;
 
 #[derive(Component)]
@@ -7,41 +9,85 @@ pub struct CollisionBox;
 
 pub fn spawn_collision_boxes(
     mut commands: Commands,
-    collision_unit_query: Query<(Entity, &UnitSize), Added<UnitSize>>,
+    asset_server: Res<AssetServer>,
+    collision_unit_query: Query<(Entity, &ColliderType), Added<ColliderType>>,
 ) {
-    for (entity, unit_size) in collision_unit_query.iter() {
-        commands.entity(entity).with_children(|parent| {
-            parent.spawn_bundle(
-                SpriteBundle {
-                    sprite: Sprite {
-                        color: Color::from([0.8, 0.4, 0.6, 0.3]),
-                        custom_size: Some(unit_size.collider_size),
-                        ..Default::default()
-                    },
-                    ..Default::default()
-                }
-            ).insert(CollisionBox);
-        });
+    for (entity, collider_type) in collision_unit_query.iter() {
+        match collider_type {
+            Circle(radius) => {
+                let size = Vec2::new(*radius * 2.0, *radius * 2.0);
+                commands.entity(entity).with_children(|parent| {
+                    parent.spawn_bundle(
+                        SpriteBundle {
+                            sprite: Sprite {
+                                color: Color::from([0.8, 0.4, 0.6, 0.3]),
+                                custom_size: Some(size),
+                                ..Default::default()
+                            },
+                            texture: asset_server.load("sprites/collider_circle.png"),
+                            ..Default::default()
+                        }
+                    ).insert(CollisionBox);
+                });
+            }
+
+            Rectangle(size) => {
+                commands.entity(entity).with_children(|parent| {
+                    parent.spawn_bundle(
+                        SpriteBundle {
+                            sprite: Sprite {
+                                color: Color::from([0.8, 0.4, 0.6, 0.3]),
+                                custom_size: Some(size.clone()),
+                                ..Default::default()
+                            },
+                            ..Default::default()
+                        }
+                    ).insert(CollisionBox);
+                });
+            }
+        }
     }
 }
 
 pub fn init_collision_boxes(
     mut commands: Commands,
-    collision_unit_query: Query<(Entity, &UnitSize), With<Transform>>,
+    asset_server: Res<AssetServer>,
+    collision_unit_query: Query<(Entity, &ColliderType), With<Transform>>,
 ) {
-    for (entity, unit_size) in collision_unit_query.iter() {
-        commands.entity(entity).with_children(|parent| {
-            parent.spawn_bundle(
-                SpriteBundle {
-                    sprite: Sprite {
-                        color: Color::from([0.8, 0.4, 0.6, 0.3]),
-                        custom_size: Some(unit_size.collider_size),
-                        ..Default::default()
-                    },
-                    ..Default::default()
-                }
-            ).insert(CollisionBox);
-        });
+    for (entity, collider_type) in collision_unit_query.iter() {
+        match collider_type {
+            Circle(radius) => {
+                let size = Vec2::new(*radius * 2.0, *radius * 2.0);
+                commands.entity(entity).with_children(|parent| {
+                    parent.spawn_bundle(
+                        SpriteBundle {
+                            sprite: Sprite {
+                                color: Color::from([0.8, 0.4, 0.6, 0.3]),
+                                custom_size: Some(size),
+                                ..Default::default()
+                            },
+                            texture: asset_server.load("sprites/collider_circle.png"),
+                            ..Default::default()
+                        }
+                    ).insert(CollisionBox);
+                });
+            }
+
+            Rectangle(size) => {
+                commands.entity(entity).with_children(|parent| {
+                    parent.spawn_bundle(
+                        SpriteBundle {
+                            sprite: Sprite {
+                                color: Color::from([0.8, 0.4, 0.6, 0.3]),
+                                custom_size: Some(size.clone()),
+                                ..Default::default()
+                            },
+                            ..Default::default()
+                        }
+                    ).insert(CollisionBox);
+                });
+            }
+        }
     }
 }
 
