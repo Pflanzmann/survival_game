80508e0cd7bdfc5a799e66d0d20ada36568a4b90
Derhartmannmann <derhartmannmann@gmail.com>
2022-05-01 17:47:42 +0200

    Add sound effect for shooting and hitting enemys

--- src/assets_handling/preload_audio_system.rs
+++ src/assets_handling/preload_audio_system.rs
@@ -7,7 +7,9 @@ use bevy_kira_audio::AudioSource;
 #[derive(Default)]
 pub struct SoundHandles {
     pub coin_pickup_sound: Handle<AudioSource>,
-    pub background_music: Handle<AudioSource>
+    pub background_music: Handle<AudioSource>,
+    pub shoot_sound: Handle<AudioSource>,
+    pub hit_sound : Handle<AudioSource>
 }
 
 pub fn preload_audio_system(
@@ -15,5 +17,7 @@ pub fn preload_audio_system(
     asset_server: Res<AssetServer>,
 ) {
     sound_handles.coin_pickup_sound = asset_server.load("audio/coin_pickup_sound.ogg");
-    sound_handles.background_music = asset_server.load("audio/background_theme.mp3")
+    sound_handles.background_music = asset_server.load("audio/background_theme.mp3");
+    sound_handles.shoot_sound = asset_server.load("audio/shoot_sound.mp3");
+    sound_handles.hit_sound = asset_server.load("audio/hit_sound.mp3");
 }
