531ea83e3f86ba0778459e391157722841c8b9b0
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-05-02 19:48:27 +0200

    Resolve issue with removing mods

--- src/units/unit_modifications/helper/mod_list_deregister_system.rs
+++ src/units/unit_modifications/helper/mod_list_deregister_system.rs
@@ -1,24 +1,24 @@
 use bevy::prelude::{EventReader, Query, With};
 
-use crate::models::events::apply_mod_to_target_event::ApplyModToTargetEvent;
+use crate::models::events::remove_mod_from_target_event::RemoveModFromTargetEvent;
 use crate::models::mod_register::ModRegister;
 use crate::models::modifications::descriptors::modification::Modification;
 
 pub fn mod_list_deregister_system(
-    mut apply_events: EventReader<ApplyModToTargetEvent>,
+    mut apply_events: EventReader<RemoveModFromTargetEvent>,
     mod_query: Query<With<Modification>>,
     mut target_query: Query<&mut ModRegister>,
 ) {
-    for apply_event in apply_events.iter() {
-        if mod_query.get(apply_event.mod_entity).is_err() {
+    for remove_event in apply_events.iter() {
+        if mod_query.get(remove_event.mod_entity).is_err() {
             continue;
         };
 
-        let mut target = match target_query.get_mut(apply_event.target_entity) {
+        let mut target = match target_query.get_mut(remove_event.target_entity) {
             Ok(target) => target,
             Err(_) => continue,
         };
 
-        target.register.remove(&apply_event.mod_entity);
+        target.register.remove(&remove_event.mod_entity);
     }
 }
