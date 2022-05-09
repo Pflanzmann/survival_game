ddce933d4f3714f841362c7ed2d58e979539665a
Derhartmannmann <derhartmannmann@gmail.com>
2022-05-09 23:39:46 +0200

    Add teleport animation

--- src/assets_handling/preload_audio_system.rs
+++ src/assets_handling/preload_audio_system.rs
@@ -9,7 +9,8 @@ pub struct SoundHandles {
     pub coin_pickup_sound: Handle<AudioSource>,
     pub background_music: Handle<AudioSource>,
     pub shoot_sound: Handle<AudioSource>,
-    pub hit_sound : Handle<AudioSource>
+    pub hit_sound : Handle<AudioSource>,
+    pub teleport_sound: Handle<AudioSource>
 }
 
 pub fn preload_audio_system(
@@ -20,4 +21,5 @@ pub fn preload_audio_system(
     sound_handles.background_music = asset_server.load("audio/background_theme.mp3");
     sound_handles.shoot_sound = asset_server.load("audio/shoot_sound.mp3");
     sound_handles.hit_sound = asset_server.load("audio/hit_sound.mp3");
+    sound_handles.teleport_sound = asset_server.load("audio/teleport.mp3");
 }
