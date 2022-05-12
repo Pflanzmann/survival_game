6ce856dd65b47b872865a62050714d6cd34b07c3
Derhartmannmann <derhartmannmann@gmail.com>
2022-05-12 22:22:50 +0200

    Update scaling in teleport animation now does delta time steps. get_close_position_2d moved to generation of teleporting component

--- src/animation/teleport_animation_system.rs
+++ src/animation/teleport_animation_system.rs
@@ -7,7 +7,6 @@ use crate::models::audio::sound_handle_channel::SoundHandleChannel;
 use crate::models::behavior::teleporting_script::TeleportingScript;
 use crate::models::player::Player;
 use crate::models::unit_size::UnitSize;
-use crate::util::get_close_position_2d::get_close_position_2d;
 
 pub fn teleport_animation_system(
     mut commands: Commands,
@@ -17,35 +16,35 @@ pub fn teleport_animation_system(
     mut unit_query: Query<(Entity, &mut Transform, &mut TeleportingScript, &mut Sprite, &mut UnitSize), Without<Player>>,
 ) {
     for (entity, mut transform, mut teleporting, mut sprite, mut unit_size) in unit_query.iter_mut() {
-        teleporting.timer += time.delta_seconds();
+        teleporting.progress += time.delta_seconds();
 
         let mut custom_size = match sprite.custom_size {
             Some(value) => value,
             None => continue,
         };
 
-        if teleporting.timer < 0.5 && custom_size.x > 0.0 {
-            custom_size.x -= 5.0;
-            custom_size.y += 10.0;
+        if teleporting.progress < (teleporting.duration / 2.0) && custom_size.x > 0.0 {
+            custom_size.x -= time.delta_seconds() / (teleporting.duration / 2.0) * unit_size.collider_size.x;
+            custom_size.y += time.delta_seconds() / (teleporting.duration / 2.0) * unit_size.collider_size.y;
             sprite.custom_size = Some(custom_size);
         }
 
-        if teleporting.timer > 0.5 && teleporting.timer < 1.0 {
+        if teleporting.progress > (teleporting.duration / 2.0) && teleporting.progress < teleporting.duration {
             if !teleporting.did_port {
-                let pos_vec = get_close_position_2d(teleporting.target_pos.x, teleporting.target_pos.y, 250.0, 400.0);
+                let pos_vec = teleporting.target_pos;
                 transform.translation.x = pos_vec[0];
                 transform.translation.y = pos_vec[1];
                 teleporting.did_port = true;
                 sound_manager.queue_sound(SoundHandleChannel::Misc(sound_handles.teleport_sound.clone()));
             }
-            custom_size.x += 5.0;
-            custom_size.y -= 10.0;
+            custom_size.x += time.delta_seconds() / (teleporting.duration / 2.0) * unit_size.collider_size.x;
+            custom_size.y -= time.delta_seconds() / (teleporting.duration / 2.0) * unit_size.collider_size.y;
             sprite.custom_size = Some(custom_size);
         }
 
-        if teleporting.timer > 1.0 {
+        if teleporting.progress > teleporting.duration {
             if !teleporting.did_port {
-                let pos_vec = get_close_position_2d(teleporting.target_pos.x, teleporting.target_pos.y, 250.0, 400.0);
+                let pos_vec = teleporting.target_pos;
                 transform.translation.x = pos_vec[0];
                 transform.translation.y = pos_vec[1];
                 teleporting.did_port = true;
