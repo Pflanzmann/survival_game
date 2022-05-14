697eb3cbc225398bacc8bbee3b5bcbd74eeec91f
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-05-14 14:13:50 +0200

    Simplify sprite rotation systems

--- src/units/sprite_aim_rotate_system.rs
+++ src/units/sprite_aim_rotate_system.rs
@@ -7,10 +7,8 @@ pub fn sprite_aim_rotate_system(
     mut sprite_query: Query<(&AimDirection, &mut Transform), (Changed<AimDirection>, With<SpriteAimRotation>)>,
 ) {
     for (aim_direction, mut transform) in sprite_query.iter_mut() {
-        let target_pos = transform.translation.truncate() + aim_direction.direction;
-        let diff = target_pos - transform.translation.truncate();
-        let angle = diff.y.atan2(diff.x);
+        let angle = aim_direction.direction.y.atan2(aim_direction.direction.x);
 
-        transform.rotation = Quat::from_axis_angle(Vec3::new(0., 0., 1.), angle);
+        transform.rotation = Quat::from_axis_angle(Vec3::new(0.0, 0.0, 1.0), angle);
     }
 }
--- src/units/sprite_move_rotate_system.rs
+++ src/units/sprite_move_rotate_system.rs
@@ -7,10 +7,8 @@ pub fn sprite_move_rotate_system(
     mut sprite_query: Query<(&MoveDirection, &mut Transform), (Changed<MoveDirection>, With<SpriteMoveRotation>)>,
 ) {
     for (move_direction, mut transform) in sprite_query.iter_mut() {
-        let target_pos = transform.translation.truncate() + move_direction.direction;
-        let diff = target_pos - transform.translation.truncate();
-        let angle = diff.y.atan2(diff.x);
+        let angle = move_direction.direction.y.atan2(move_direction.direction.x);
 
-        transform.rotation = Quat::from_axis_angle(Vec3::new(0., 0., 1.), angle);
+        transform.rotation = Quat::from_axis_angle(Vec3::new(0.0, 0.0, 1.0), angle);
     }
 }
