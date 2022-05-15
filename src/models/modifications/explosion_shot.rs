1458f86caccbb095073584106814f8d0de3182ce
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-05-15 21:38:14 +0200

    Implement proc chance for explosion shot

--- src/models/modifications/explosion_shot.rs
+++ src/models/modifications/explosion_shot.rs
@@ -4,6 +4,7 @@ use serde::Deserialize;
 #[derive(Clone, Component, Deserialize)]
 pub struct ExplosionShot {
     pub explosion_sprite_path: String,
+    pub explosion_chance: f32,
     pub radius: f32,
     pub explosion_time_alive: f32,
 }
