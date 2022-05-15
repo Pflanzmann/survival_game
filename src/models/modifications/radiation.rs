675d5132d2188d623489d2631ecf65c0d0f3a6cd
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-05-15 23:33:53 +0200

    Extrude radiation data into mod

--- src/models/modifications/radiation.rs
+++ src/models/modifications/radiation.rs
@@ -1,8 +1,13 @@
 use bevy::ecs::component::Component;
+use bevy::prelude::Vec2;
 use serde::Deserialize;
 
 #[derive(Copy, Clone, Component, Deserialize)]
-pub struct Radiation;
+pub struct Radiation {
+    pub unit_size: Vec2,
+    pub damage: f32,
+    pub damage_interval: f32,
+}
 
 #[derive(Component)]
 pub struct RadiationUnit;
