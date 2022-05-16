d2714697567286af221248fbfb6bca3147fb4ef1
Derhartmannmann <derhartmannmann@gmail.com>
2022-05-16 21:25:37 +0200

    Refactor satisfy reviewer a little

--- src/animation/animation_state_handle_system.rs
+++ src/animation/animation_state_handle_system.rs
@@ -1,11 +1,10 @@
-use bevy::prelude::{Changed, Query, Vec2, With};
+use bevy::prelude::{Changed, Query, Vec2};
 
 use crate::models::animation::animation_state::{AnimationState, CurrentAnimationState};
 use crate::models::move_direction::MoveDirection;
-use crate::models::player::Player;
 
 pub fn animation_state_handle_system(
-    mut player_query: Query<(&mut CurrentAnimationState, &MoveDirection), (Changed<MoveDirection>, With<Player>)>
+    mut player_query: Query<(&mut CurrentAnimationState, &MoveDirection), Changed<MoveDirection>>
 ) {
     for (mut state, direction) in player_query.iter_mut() {
         if direction.direction == Vec2::new(0.0, 0.0) {
--- src/animation/movement_animation_side_system.rs
+++ src/animation/movement_animation_side_system.rs
@@ -15,15 +15,8 @@ pub fn movement_animation_side_system(
             continue;
         }
 
-        match state.state {
-            AnimationState::WalkSide => {
-                animation_data.progress += time.delta_seconds() * speed.get_total_amount();
-                sprite.index = (animation_data.progress as usize % animation_data.framecount) + (4 * animation_data.atlas_row);
-            }
-            AnimationState::WalkUp => {}
-            AnimationState::WalkDown => {}
-            AnimationState::Idle => {}
-        }
+        animation_data.progress += time.delta_seconds() * speed.get_total_amount();
+        sprite.index = (animation_data.progress as usize % animation_data.framecount) + (4 * animation_data.atlas_row);
     }
 }
 
