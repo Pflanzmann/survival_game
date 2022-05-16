b5caf963549b080ff71fc42d96da2c86fb3fda17
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-05-16 11:29:40 +0200

    Rename KnockBackShot to KnockBack

--- src/models/modifications/knock_back.rs
+++ src/models/modifications/knock_back.rs
@@ -2,7 +2,7 @@ use bevy::ecs::component::Component;
 use serde::Deserialize;
 
 #[derive(Copy, Clone, Component, Deserialize)]
-pub struct KnockBackShot {
+pub struct KnockBack {
     pub push_duration: f32,
     pub push_force: f32,
 }
