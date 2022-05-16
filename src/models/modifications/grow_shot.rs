49aa47da7bf779a2300283d7875ad04cfde07031
Lord Lothar Neumann <lotharneumann90@web.de>
2022-05-16 23:19:15 +0200

    Change GrowShot to apply affect per second

--- src/models/modifications/grow_shot.rs
+++ src/models/modifications/grow_shot.rs
@@ -3,6 +3,6 @@ use serde::Deserialize;
 
 #[derive(Copy, Clone, Component, Deserialize)]
 pub struct GrowShot {
-    pub size_step: f32,
-    pub damage_step: f32,
+    pub damage_per_second: f32,
+    pub size_per_second: f32,
 }
