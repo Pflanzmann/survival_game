dd08313b70e22f9b9fa4d1ceb83263a13bdf6375
Lord Lothar Neumann <lotharneumann90@web.de>
2022-05-16 23:19:15 +0200

    Change GrowShot to apply affect per second

--- src/units/modifications/bullet_modifications/grow_shot_system.rs
+++ src/units/modifications/bullet_modifications/grow_shot_system.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::{Query, With};
+use bevy::prelude::{Query, Res, Time, With};
 
 use crate::models::modifications::grow_shot::GrowShot;
 use crate::models::unit_attributes::attribute::*;
@@ -9,10 +9,11 @@ use crate::models::unit_attributes::unit_size::UnitSize;
 /// A system to grow the [Bullet] that has [GrowShot] applied to it.
 /// This increases the [UnitSize] and [Damage] of the bullet depending on the distance traveled.
 pub fn grow_shot_system(
+    time: Res<Time>,
     mut bullet_query: Query<(&mut UnitSize, &mut Damage, &GrowShot), With<TravelRange>>,
 ) {
     for (mut unit_size, mut damage, grow_shot) in bullet_query.iter_mut() {
-        unit_size.add_bonus_amount(grow_shot.size_step);
-        damage.add_bonus_amount(grow_shot.damage_step);
+        damage.add_bonus_amount(grow_shot.damage_per_second * time.delta_seconds());
+        unit_size.add_bonus_amount(grow_shot.size_per_second * time.delta_seconds());
     }
 }
