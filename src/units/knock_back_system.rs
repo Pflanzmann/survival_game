c4c07ae03558c97463a6b7a9efb188ef1e22a177
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-05-18 23:24:18 +0200

    Replace wrong component for knock_back_system

--- src/units/knock_back_system.rs
+++ src/units/knock_back_system.rs
@@ -1,25 +1,25 @@
 use bevy::prelude::{Commands, EventReader, Query};
 
 use crate::models::events::damaged_event::DamagedEvent;
-use crate::models::modifications::knock_back_shot::KnockBackShot;
+use crate::models::knock_back::KnockBack;
 use crate::models::move_direction::MoveDirection;
 use crate::models::unit_push::UnitPush;
 
 pub fn knock_back_system(
     mut commands: Commands,
     mut hit_event: EventReader<DamagedEvent>,
-    source_query: Query<(&MoveDirection, &KnockBackShot)>,
+    source_query: Query<(&MoveDirection, &KnockBack)>,
 ) {
     for event in hit_event.iter() {
-        let (move_direction, knock_back_shot) = match source_query.get(event.source_entity) {
+        let (move_direction, knock_back) = match source_query.get(event.source_entity) {
             Ok(source) => source,
             Err(_) => continue,
         };
 
         commands.entity(event.target_entity).insert(UnitPush {
             direction: move_direction.direction,
-            duration: knock_back_shot.push_duration,
-            force: knock_back_shot.push_force,
+            duration: knock_back.push_duration,
+            force: knock_back.push_force,
         });
     }
 }
