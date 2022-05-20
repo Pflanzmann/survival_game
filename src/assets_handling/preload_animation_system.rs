afd37f58a514dc3176e35770244041b79d218312
Derhartmannmann <derhartmannmann@gmail.com>
2022-05-20 22:15:33 +0200

    Remove idle animation is no longer preloaded as texture atlas

--- src/assets_handling/preload_animation_system.rs
+++ src/assets_handling/preload_animation_system.rs
@@ -13,7 +13,6 @@ pub fn preload_animation_system(
     mut atlas_handles: ResMut<AtlasHandles>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
-    atlas_handles.player_idle_atlas = texture_atlases.add(TextureAtlas::from_grid(asset_server.load("sprite_sheets/ranger_idle.png"), Vec2::new(32.0, 32.0), 2, 1));
     atlas_handles.player_atlas = texture_atlases.add(TextureAtlas::from_grid(asset_server.load("sprite_sheets/hero1.png"), Vec2::new(16.0, 16.0), 4, 6));
     atlas_handles.goblin_atlas = texture_atlases.add(TextureAtlas::from_grid(asset_server.load("sprite_sheets/goblin.png"), Vec2::new(64.0, 80.0), 4, 4));
 }
