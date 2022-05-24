05a0e207c5068538850861081d59b80671839105
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-05-24 22:41:18 +0200

    Change information of a SpawnTask

--- src/models/spawner/spawn_task.rs
+++ src/models/spawner/spawn_task.rs
@@ -1,15 +1,13 @@
-use bevy::math::Vec3;
+use bevy::prelude::{Entity, Vec2};
 
 pub struct SpawnTask {
-    position: Vec3,
+    pub enemy_config_index: usize,
+    pub position: Vec2,
+    pub target_player: Entity,
 }
 
 impl SpawnTask {
-    pub fn new(position: Vec3) -> Self {
-        SpawnTask { position }
-    }
-
-    pub fn get_position(&self) -> Vec3 {
-        self.position
+    pub fn new(enemy_config_index: usize, position: Vec2, target_player: Entity) -> Self {
+        SpawnTask { enemy_config_index, position, target_player }
     }
 }
