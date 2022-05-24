585f1ba459d8a028a29bd36ea5b91cd5be04059b
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-05-25 00:28:29 +0200

    Add different monsters to different phases

--- src/spawner/spawn_scheduler_system.rs
+++ src/spawner/spawn_scheduler_system.rs
@@ -13,30 +13,40 @@ pub fn spawn_scheduler_system(
     mut spawn_interval_timer: ResMut<SpawnIntervalTimer>,
     mut spawn_phase_timer: ResMut<SpawnPhaseTimer>,
     mut spawn_task_receiver: ResMut<SpawnTaskReceiver>,
-    player_query: Query<(Entity, &Transform), With<Player>>,
     stage_spawn_behavior_handle: Res<StageSpawnBehaviorHandle>,
+    player_query: Query<(Entity, &Transform), With<Player>>,
 ) {
-    spawn_phase_timer.timer -= time.delta().as_secs_f32();
-    if spawn_interval_timer.timer < 0.0 {
+    spawn_phase_timer.timer -= time.delta_seconds();
+    if spawn_phase_timer.timer < 0.0 {
         spawn_phase_timer.current_spawn_phase += 1;
+        if spawn_phase_timer.current_spawn_phase >= stage_spawn_behavior_handle.default_spawn_phase.spawn_phases.len() {
+            spawn_phase_timer.current_spawn_phase = 0;
+        }
+
         spawn_phase_timer.timer = stage_spawn_behavior_handle.default_spawn_phase.spawn_phases[spawn_phase_timer.current_spawn_phase].duration;
+        spawn_interval_timer.timer = stage_spawn_behavior_handle.default_spawn_phase.spawn_phases[spawn_phase_timer.current_spawn_phase].spawn_interval;
     }
 
-    spawn_interval_timer.timer -= time.delta().as_secs_f32();
+    spawn_interval_timer.timer -= time.delta_seconds();
     if spawn_interval_timer.timer > 0.0 {
         return;
     }
     spawn_interval_timer.timer = stage_spawn_behavior_handle.default_spawn_phase.spawn_phases[spawn_phase_timer.current_spawn_phase].spawn_interval;
 
     for (player_entity, player_transform) in player_query.iter() {
-        let enemy_index = rand::thread_rng().gen_range(0..stage_spawn_behavior_handle.default_spawn_phase.spawn_phases[spawn_phase_timer.current_spawn_phase].enemies.len());
+        if stage_spawn_behavior_handle.default_spawn_phase.spawn_phases[spawn_phase_timer.current_spawn_phase].enemies.is_empty() {
+            return;
+        }
+
+        let enemy = rand::thread_rng().gen_range(0..stage_spawn_behavior_handle.default_spawn_phase.spawn_phases[spawn_phase_timer.current_spawn_phase].enemies.len());
+        let enemy_index = stage_spawn_behavior_handle.default_spawn_phase.spawn_phases[spawn_phase_timer.current_spawn_phase].enemies[enemy].enemy_index;
 
         let random_x = random::<f32>() * 2.0 - 1.0;
         let random_y = random::<f32>() * 2.0 - 1.0;
 
         let direction_to_spawn = Vec2::new(random_x, random_y).normalize_or_zero();
 
-        let position_to_spawn = player_transform.translation.truncate() + (direction_to_spawn * (256.0 * 12.0));
+        let position_to_spawn = player_transform.translation.truncate() + (direction_to_spawn * (256.0 * 15.0));
         spawn_task_receiver.push_new_task(SpawnTask::new(enemy_index, position_to_spawn, player_entity));
     }
 }
