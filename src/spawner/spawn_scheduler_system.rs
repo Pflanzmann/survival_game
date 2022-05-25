a83e02caacc8a0509236a68ba48d08eaaafcf66c
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-05-25 20:01:51 +0200

    Implement correct usage of spawn_weight

--- src/spawner/spawn_scheduler_system.rs
+++ src/spawner/spawn_scheduler_system.rs
@@ -34,19 +34,23 @@ pub fn spawn_scheduler_system(
     spawn_interval_timer.timer = stage_spawn_behavior_handle.default_spawn_phase.spawn_phases[spawn_phase_timer.current_spawn_phase].spawn_interval;
 
     for (player_entity, player_transform) in player_query.iter() {
-        if stage_spawn_behavior_handle.default_spawn_phase.spawn_phases[spawn_phase_timer.current_spawn_phase].enemies.is_empty() {
-            return;
+        let total_spawn_weight: f32 = stage_spawn_behavior_handle.default_spawn_phase.spawn_phases[spawn_phase_timer.current_spawn_phase].enemies.iter().map(|value| value.spawn_weight).sum();
+        let random_number = random::<f32>() * total_spawn_weight;
+
+        let mut previous_spawn_weights: f32 = 0.0;
+        for enemy in stage_spawn_behavior_handle.default_spawn_phase.spawn_phases[spawn_phase_timer.current_spawn_phase].enemies.iter() {
+            previous_spawn_weights += enemy.spawn_weight;
+
+            if random_number < previous_spawn_weights {
+                let random_x = random::<f32>() * 2.0 - 1.0;
+                let random_y = random::<f32>() * 2.0 - 1.0;
+
+                let direction_to_spawn = Vec2::new(random_x, random_y).normalize_or_zero();
+
+                let position_to_spawn = player_transform.translation.truncate() + (direction_to_spawn * (256.0 * 15.0));
+                spawn_task_receiver.push_new_task(SpawnTask::new(enemy.enemy_index, position_to_spawn, player_entity));
+                return;
+            }
         }
-
-        let enemy = rand::thread_rng().gen_range(0..stage_spawn_behavior_handle.default_spawn_phase.spawn_phases[spawn_phase_timer.current_spawn_phase].enemies.len());
-        let enemy_index = stage_spawn_behavior_handle.default_spawn_phase.spawn_phases[spawn_phase_timer.current_spawn_phase].enemies[enemy].enemy_index;
-
-        let random_x = random::<f32>() * 2.0 - 1.0;
-        let random_y = random::<f32>() * 2.0 - 1.0;
-
-        let direction_to_spawn = Vec2::new(random_x, random_y).normalize_or_zero();
-
-        let position_to_spawn = player_transform.translation.truncate() + (direction_to_spawn * (256.0 * 15.0));
-        spawn_task_receiver.push_new_task(SpawnTask::new(enemy_index, position_to_spawn, player_entity));
     }
 }
