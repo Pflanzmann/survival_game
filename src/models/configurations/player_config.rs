e078f5c2efd677ec2120a28f129b4433829a6ac4
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-05-25 22:55:50 +0200

    Sort sprite folder

--- src/models/configurations/player_config.rs
+++ src/models/configurations/player_config.rs
@@ -4,7 +4,6 @@ use serde::Deserialize;
 pub struct PlayerConfig {
     pub sprite_custom_size_x: f32,
     pub sprite_custom_size_y: f32,
-    pub sprite_path: String,
     pub move_speed: f32,
     pub damage: f32,
     pub health: f32,
