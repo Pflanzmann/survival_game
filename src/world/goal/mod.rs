4911153c91e0597a0cab44b5f50804a5e2ee73fe
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-06-01 22:58:09 +0200

    Run goal system only in game state

--- src/world/goal/mod.rs
+++ src/world/goal/mod.rs
@@ -1,5 +1,6 @@
-use bevy::prelude::{App, Plugin};
+use bevy::prelude::{App, Plugin, SystemSet};
 
+use crate::AppState;
 use crate::world::goal::goal_activation_system::goal_activation_system;
 use crate::world::goal::setup_goal_system::setup_goal_system;
 
@@ -11,8 +12,11 @@ pub struct GoalPlugin;
 impl Plugin for GoalPlugin {
     fn build(&self, app: &mut App) {
         app
-            .add_startup_system(setup_goal_system)
-            .add_system(goal_activation_system)
+            .add_system_set(
+                SystemSet::on_update(AppState::InGame)
+                    .with_system(setup_goal_system)
+                    .with_system(goal_activation_system)
+            )
         ;
     }
 }
